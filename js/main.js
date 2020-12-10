b6246ff3340cd093508c84093bbaef76375c7ee5
agardunocentro <47249877+agardunocentro@users.noreply.github.com>
2020-12-10 15:09:30 -0600

    10122020-15:09

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// const el = document.querySelector(".categoria__nav div");
+// const observer = new IntersectionObserver(
+//   ([e]) => e.target.classList.toggle("pinned", e.intersectionRatio < 1),
+//   { threshold: [1] }
+// );
+
+// observer.observe(el);
